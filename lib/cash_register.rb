1999dbf6a53d652173db625d43c16a3e5d7b7440
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-26 04:31:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,17 @@
 class CashRegister
 attr_accessor :total
+<<<<<<< HEAD
+=======
+items = []
+>>>>>>> 8c169726b90b8d5b2478a18d69e8876d6aecfc3f
 
 def initialize(discount=0)
   @total = 0
   @discount = discount
+<<<<<<< HEAD
   @items = []
+=======
+>>>>>>> 8c169726b90b8d5b2478a18d69e8876d6aecfc3f
 end
 
 
@@ -16,12 +23,16 @@ def add_item(title,price, quantity=1)
   @quantity = quantity
   @total += price*quantity
   @previoustotal = @total
+<<<<<<< HEAD
 
 num = 0
   while num < quantity
     @items << title
     num += 1
   end
+=======
+  items << title
+>>>>>>> 8c169726b90b8d5b2478a18d69e8876d6aecfc3f
 end
 
 def apply_discount
@@ -35,7 +46,11 @@ end
 end
 
 def items
+<<<<<<< HEAD
   @items
+=======
+  items
+>>>>>>> 8c169726b90b8d5b2478a18d69e8876d6aecfc3f
 end
 
 def void_last_transaction
